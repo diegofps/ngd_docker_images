35eeac02b64220e2c2bf4e1fb888a1f8d25dedf7
Diego Souza <diego@wespa.com.br>
2021-04-06 19:40:07 -0700

    Making pkg_deploy.sh deploy run_*.sh scripts

--- images/hibench/pkg/pkg_deploy.sh
+++ images/hibench/pkg/pkg_deploy.sh
@@ -6,7 +6,10 @@ sudo kubectl exec -it bigdata2-primary -- rm /app/automl-tunner_2.12-1.0.jar
 
 echo "\nSending new copies..."
 sudo kubectl cp ./target/scala-2.12/automl-tunner_2.12-1.0.jar bigdata2-primary:/app
-sudo kubectl cp ./run_submits.sh bigdata2-primary:/app
+sudo kubectl cp ./run_hibench.sh bigdata2-primary:/app
+sudo kubectl cp ./run_sparkpi.sh bigdata2-primary:/app
+sudo kubectl cp ./run_automl.sh bigdata2-primary:/app
+
 sudo kubectl exec -it bigdata2-primary -- hadoop fs -put /app/automl-tunner_2.12-1.0.jar hdfs://bigdata2-primary:9000/automl-tunner_2.12-1.0.jar
 sudo kubectl exec -it bigdata2-primary -- hdfs dfs -setrep 3 hdfs://bigdata2-primary:9000/automl-tunner_2.12-1.0.jar
 
