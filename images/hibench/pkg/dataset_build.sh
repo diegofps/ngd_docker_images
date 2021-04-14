5ac0d70fbf3ee905de1c008275fe82b42f51daa8
Diego Souza <diego@wespa.com.br>
2021-04-14 14:53:50 -0700

    Adding scripts to execute experiment4, multidataset, with regression algorithms

--- images/hibench/pkg/dataset_build.sh
+++ images/hibench/pkg/dataset_build.sh
@@ -1,14 +1,40 @@
 #!/bin/sh
 
+
+SIZE=$1
+DIMS=$2
+IDD=$3
+
+if [ "$SIZE" = "" ]; then
+  SIZE=1
+fi
+
+if [ "$DIMS" = "" ]; then
+  DIMS=20
+fi
+
+if [ "$IDD" = "" ]; then
+  IDD="generic"
+fi
+
+SAMPLES_CLASSIFICATION=$(( SIZE * 2500 ))
+SAMPLES_REGRESSION=$(( SIZE * 10000 ))
+
+NAME_CLASSIFICATION="classification_dataset_${IDD}"
+NAME_REGRESSION="regression_dataset_${IDD}"
+
+
 echo "Creating datasets"
-ml_dataset_classification_create.py 5000 30 2 0.25 2 ./classification_dataset.csv
-ml_dataset_regression_create.py 10 0.1 5000 ./regression_dataset.csv
+ml_dataset_classification_create.py $SAMPLES_CLASSIFICATION $DIMS 2 0.25 2 "./${NAME_CLASSIFICATION}.csv"
+ml_dataset_regression_create.py $DIMS 0.1 $SAMPLES_REGRESSION "./${NAME_REGRESSION}.csv"
 
 echo "Converting datasets to libsvm format"
-ml_dataset_csv2libsvm.py ./classification_dataset.csv ./classification_dataset.libsvm
-ml_dataset_csv2libsvm.py ./regression_dataset.csv ./regression_dataset.libsvm
+ml_dataset_csv2libsvm.py "./${NAME_CLASSIFICATION}.csv" "./${NAME_CLASSIFICATION}.libsvm"
+ml_dataset_csv2libsvm.py "./${NAME_REGRESSION}.csv" "./${NAME_REGRESSION}.libsvm"
 
-echo "Removing dataset int original format"
-rm classification_dataset.csv
-rm regression_dataset.csv
+echo "Removing dataset in original format"
+rm "${NAME_CLASSIFICATION}.csv"
+rm "${NAME_REGRESSION}.csv"
+
+
 
